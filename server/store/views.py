15d9e8cefd33ba8ba1e18c4fec8d0fe74737df69
mahmoud-oraby <mahmoudoraby462002@gmail.com>
2023-11-20 22:04:26 +0200

    create filter in main products

--- server/store/views.py
+++ server/store/views.py
@@ -100,6 +100,7 @@ from .serializers import (
     WishListSerializer,
     ImageSerializer,
 )
+from django.db.models import Q
 
 
 class BrandViewSet(viewsets.ModelViewSet):
@@ -136,14 +137,78 @@ class ProductViewSet(viewsets.ModelViewSet):
             return ProductDetailSerializer
         return ProductListSerializer
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # Apply filters based on query parameters
+        color = self.request.query_params.get('color')
+        price_min = self.request.query_params.get('price_min')
+        price_max = self.request.query_params.get('price_max')
+        brand = self.request.query_params.get('brand')
+        category = self.request.query_params.get('category')
+        size = self.request.query_params.get('size')
+
+        # Build filter expressions
+        filters = Q()
+
+        if color:
+            filters &= Q(colors__name=color.title())
+
+        if price_min:
+            filters &= Q(price__gte=price_min)
+
+        if price_max:
+            filters &= Q(price__lte=price_max)
+
+        if brand:
+            filters &= Q(brand__name=brand.title())
+
+        if category:
+            filters &= Q(category__name=category.title())
+
+        if size:
+            filters &= Q(sizes__size=size.upper())
+
+        return queryset.filter(filters)
+
 
 class BestSellerView(generics.ListAPIView):
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().order_by("-selled")
     serializer_class = ProductListSerializer
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        return queryset.order_by("-selled")
+
+        # Apply filters based on query parameters
+        color = self.request.query_params.get('color')
+        price_min = self.request.query_params.get('price_min')
+        price_max = self.request.query_params.get('price_max')
+        brand = self.request.query_params.get('brand')
+        category = self.request.query_params.get('category')
+        size = self.request.query_params.get('size')
+
+        # Build filter expressions
+        filters = Q()
+
+        if color:
+            filters &= Q(colors__name=color.title())
+
+        if price_min:
+            filters &= Q(price__gte=price_min)
+
+        if price_max:
+            filters &= Q(price__lte=price_max)
+
+        if brand:
+            filters &= Q(brand__name=brand.title())
+
+        if category:
+            filters &= Q(category__name=category.title())
+
+        if size:
+            filters &= Q(sizes__size=size.upper())
+
+        return queryset.filter(filters)
 
 
 class WishListViewSet(viewsets.ModelViewSet):

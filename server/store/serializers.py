f70d5b4df820f8e083af114aae91264403efede2
Mahmoud Orabi <101055442+mahmoud-oraby@users.noreply.github.com>
2023-11-23 17:28:28 +0200

    Update serializers.py

--- server/store/serializers.py
+++ server/store/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from cart.models import CartItem
 from .models import (Product, Brand, Category, Color,
-                     Size, Evaluation, WishList, Image)
+                     Size, Evaluation, Image)
 from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 from rest_framework.utils import model_meta

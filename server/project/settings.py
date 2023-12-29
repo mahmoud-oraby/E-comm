162bccfba2144ec5c755a5d4f251d46a24708594
mahmoud-oraby <mahmoudoraby462002@gmail.com>
2023-12-30 01:37:39 +0200

    .

--- server/project/settings.py
+++ server/project/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 AUTH_USER_MODEL = 'authentication.User'

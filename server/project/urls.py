26d0c9b52f89f379d053a4e37eb4cf0c97622d85
mahmoud-oraby <mahmoudoraby462002@gmail.com>
2023-12-30 02:46:35 +0200

    try fix media error

--- server/project/urls.py
+++ server/project/urls.py
@@ -53,4 +53,5 @@ urlpatterns = [
          cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
          cache_timeout=0), name='schema-redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

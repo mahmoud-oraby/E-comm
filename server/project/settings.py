3a87fe09be801fbbd7f3dab07964e5bec91a7be9
mahmoud-oraby <mahmoudoraby462002@gmail.com>
2023-12-30 02:29:04 +0200

    test deploy

--- server/project/settings.py
+++ server/project/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG','True')=='True'
 
 
 AUTH_USER_MODEL = 'authentication.User'
